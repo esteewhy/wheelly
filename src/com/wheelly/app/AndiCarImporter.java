b9e65fe7a2d6b3d6df19af4eca47d1e5ef857228
esteewhy <esteewhy@localhost>
2011-05-28 10:29:35 +0300

    Implemented refuels import from AndiCar

--- src/com/wheelly/app/AndiCarImporter.java
+++ src/com/wheelly/app/AndiCarImporter.java
@@ -6,11 +6,18 @@ import java.util.Collections;
 import java.util.List;
 
 import com.wheelly.R;
+import com.wheelly.db.DatabaseHelper;
+import com.wheelly.db.HeartbeatRepository;
+import com.wheelly.db.RefuelRepository;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.provider.BaseColumns;
 import android.widget.Toast;
 
 public final class AndiCarImporter {
@@ -52,18 +59,107 @@ public final class AndiCarImporter {
 		}
 	}
 	
+	final static short Theirs = 0;
+	final static short Mine = 1;
+	final static short WE_GO = 1;
+	final static short THEY_GO = 2;
+	
 	public boolean importFromFile(String file) {
-		SQLiteDatabase db = null;
+		final SQLiteDatabase[] db = { null, null };
+		
 		try {
-			db = SQLiteDatabase.openDatabase(new File(Environment.getExternalStorageDirectory().getPath() + "/andicar/backups/" + file).getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
+			db[Theirs] = SQLiteDatabase.openDatabase(new File(Environment.getExternalStorageDirectory().getPath() + "/andicar/backups/" + file).getAbsolutePath(), null, SQLiteDatabase.OPEN_READONLY);
+			db[Mine] = new DatabaseHelper(context).getWritableDatabase();
 			
-			//db.query(table, columns, selection, selectionArgs, groupBy, having, orderBy)
+			importRefuels(db);
+			
 		} finally {
-			if(null != db) {
-				db.close();
-			}
+			for(SQLiteDatabase i : db) if(i != null) i.close();
 		}
 		
 		return false;
+	}
+	
+	private void importRefuels(SQLiteDatabase[] db) {
+		final Cursor[] cursor = {
+			db[Theirs].query("CAR_REFUEL",
+				"CarIndex,Quantity,Price,DATETIME(Date, 'unixepoch') AS Date,Amount".split(","),
+				null, null, null, null, "CarIndex DESC"),
+			db[Mine].rawQuery(
+				"SELECT h.odometer FROM heartbeats h"
+				+ " INNER JOIN refuels r ON r.heartbeat_id = h." + BaseColumns._ID
+				+ " ORDER BY h.odometer DESC", null)
+		};
+		
+		long[] current = { 0, 0 };
+		short move = WE_GO | THEY_GO;
+		boolean commit = false;
+		
+		while(true) {
+			if(commit) {
+				commit = false;
+				importRefuel(cursor[Theirs], db[Mine]);
+			}
+			
+			if((move & THEY_GO) != 0) {
+				if(cursor[Theirs].moveToNext()) {
+					current[Theirs] = cursor[Theirs].getLong(cursor[Theirs].getColumnIndexOrThrow("CarIndex"));
+				} else break;
+			}
+			
+			if((move & WE_GO) != 0) {
+				if(cursor[Mine].moveToNext()) {
+					current[Mine] = cursor[Mine].getLong(cursor[Mine].getColumnIndexOrThrow("odometer"));
+				} else {
+					commit = true;
+					move = WE_GO | THEY_GO;
+					continue;
+				}
+			}
+			
+			if(current[Theirs] > current[Mine]) {
+				move = THEY_GO;
+			} else if(current[Theirs] == current[Mine]) {
+				move = WE_GO | THEY_GO;
+			} else {
+				if(move == THEY_GO) {
+					commit = true;
+				}
+				
+				move = WE_GO;
+			}
+		}
+		
+		for(Cursor i : cursor) i.close();
+	}
+	
+	private static ContentValues deserializeTheirs(Cursor cursor) {
+		ContentValues values = new ContentValues();
+		values.put("CarIndex",	cursor.getLong(cursor.getColumnIndexOrThrow("CarIndex")));
+		values.put("Quantity",	cursor.getFloat(cursor.getColumnIndexOrThrow("Quantity")));
+		values.put("Amount",	cursor.getFloat(cursor.getColumnIndexOrThrow("Amount")));
+		values.put("Date",		cursor.getString(cursor.getColumnIndexOrThrow("Date")));
+		values.put("Price",		cursor.getFloat(cursor.getColumnIndexOrThrow("Price")));
+		return values;
+	}
+	
+	private void importRefuel(Cursor cursor, SQLiteDatabase db) {
+		final long full_tank = PreferenceManager.getDefaultSharedPreferences(context).getInt("fuel_capacity", 60);
+		
+		final ContentValues values = deserializeTheirs(cursor);
+		
+		final ContentValues heartbeat = new ContentValues();
+		heartbeat.put("_created",	values.getAsString("Date"));
+		heartbeat.put("odometer",	values.getAsLong("CarIndex"));
+		heartbeat.put("fuel",		full_tank);
+		
+		final ContentValues refuel = new ContentValues();
+		refuel.put("heartbeat_id",	new HeartbeatRepository(db).insert(heartbeat));
+		refuel.put("amount",		values.getAsFloat("Quantity"));
+		refuel.put("unit_price",	values.getAsFloat("Price"));
+		refuel.put("cost",			values.getAsFloat("Amount"));
+		refuel.put("name",			"imported from AndiCar");
+		
+		new RefuelRepository(db, context).insert(refuel);
 	}
 }
