3b2153db980a260c99f84ff0640f9463dad6d49d
esteewhy <esteewhy@localhost>
2012-04-11 18:46:42 +0300

    * Taking another approach in leveraging MyTracks GDocs logic

---
+++ mytracks-reference.cmd
@@ -0,0 +1,1 @@
+"%ProgramFiles(x86)%\android\android-sdk\platform-tools\aapt" package -f -m --auto-add-overlay -M AndroidManifest.xml -S ..\mytracks\MyTracks\res -S res -J gen -I "%ProgramFiles(x86)%\Android\android-sdk\platforms\android-14\android.jar" -v
